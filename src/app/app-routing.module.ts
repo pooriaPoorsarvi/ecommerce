5a99a9aae4d597fe7768ba514a32a72c5865f723
pooria <pooriapoorsarvi@gmail.com>
2019-04-18 20:00:51 +0430

    0.3.1 Data Models

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'home', loadChildren : "./main/main-router.module.ts#MainRouterModule"},
+  {path: 'home', loadChildren : "./main/main-router.module#MainRouterModule"},
   {path : "**", redirectTo:'home'},
 ];
 
