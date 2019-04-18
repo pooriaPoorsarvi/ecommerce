d7b8e93230dbe9b3b944484a97fe35ed4625f83f
pooria <pooriapoorsarvi@gmail.com>
2019-04-18 19:33:05 +0430

    0.3.0 Data Models

--- src/app/dataModules/Promo.model.ts
+++ src/app/dataModules/Promo.model.ts
@@ -1,8 +1,13 @@
+import { ProductModel } from './Product.model';
 
 
 export class PromoModel{
   constructor(public name : string,
+              public product : ProductModel,
               public percent : number,
+              public end_date : Date,
+              // The following is for the promos which has dedicated pictures for promoting them
+              public imageAddress? : string,
               ){}
 }
 
